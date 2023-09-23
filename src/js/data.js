331baef1309b36530cfc58999b975def40697407
olga <buleka>
2023-09-23 15:11:42 +0300

    add data array

---
+++ src/js/data.js
@@ -0,0 +1,105 @@
+const dataUsers = [{
+  'name': 'Екатерина',
+  'age': 41,
+  'type': 'pending',
+  'employed': true
+},
+  {
+    'name': 'Ольга',
+    'age': 18,
+    'type': 'active',
+    'employed': false
+  },
+  {
+    'name': 'Никита',
+    'age': 38,
+    'type': 'cancelled',
+    'employed': false
+  },
+  {
+    'name': 'Иван',
+    'age': 23,
+    'type': 'subscribed',
+    'employed': true
+  },
+  {
+    'name': 'Петр',
+    'age': 38,
+    'type': 'subscribed',
+    'employed': true
+  },
+  {
+    'name': 'Андрей',
+    'age': 12,
+    'type': 'active',
+    'employed': true
+  },
+  {
+    'name': 'Андрей',
+    'age': 39,
+    'type': 'active',
+    'employed': false
+  },
+  {
+    'name': 'Дмитрий',
+    'age': 42,
+    'type': 'subscribed',
+    'employed': true
+  },
+  {
+    'name': 'Даниил',
+    'age': 43,
+    'type': 'active',
+    'employed': true
+  },
+  {
+    'name': 'Даниил',
+    'age': 15,
+    'type': 'cancelled',
+    'employed': true
+  },
+  {
+    'name': 'Юлия',
+    'age': 10,
+    'type': 'active',
+    'employed': true
+  },
+  {
+    'name': 'Роман',
+    'age': 23,
+    'type': 'pending',
+    'employed': true
+  },
+  {
+    'name': 'Дмитрий',
+    'age': 34,
+    'type': 'subscribed',
+    'employed': true
+  },
+  {
+    'name': 'Наталья',
+    'age': 32,
+    'type': 'active',
+    'employed': false
+  },
+  {
+    'name': 'Евгения',
+    'age': 11,
+    'type': 'active',
+    'employed': false
+  },
+  {
+    'name': 'Даниил',
+    'age': 29,
+    'type': 'cancelled',
+    'employed': true
+  },
+  {
+    'name': 'Сергей',
+    'age': 25,
+    'type': 'pending',
+    'employed': false
+  }
+];
+
+export default dataUsers;
